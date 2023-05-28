2f993aa3c8423002915218b6c7a3414fd46ddd67
JacopoBiagioni <biagioni.jacopo@iisgalvanimi.edu.it>
2023-05-28 15:17:21 +0000

    commit D

--- VerificaA/app.py
+++ VerificaA/app.py
@@ -8,6 +8,15 @@ import random
 import geojson
 import shapely.wkt
 
+# pip install Flask-PyMongo
+# pip install Flask
+# pip install dnspython
+# npm install -g --force nodemon
+# pip install flask-cors
+# pip install geojson
+# pip install shapely
+# pip install jsonify
+# npm i -g nodemon
 
 app = Flask(__name__)
 
@@ -17,13 +26,29 @@ app.config["MONGO_URI"] = "mongodb+srv://jbiagioni:M7JYoNondmtLqBJk@atlascluster
 mongo = PyMongo(app)
 CORS(app)
 tane = mongo.db.tane
-#output = []
-#for s in tane.find():
-#    output.append({"lat": s['lat'], "lng": s['lng']})
+output = []
+for s in tane.find():
+    output.append({"lat": s['lat'], "lng": s['lng']})
 
 @app.route("/all")
 def all():
-    return send_file('coordinate.json')
+    return (output)
+
+@app.route("/raffaello")
+def raffaello():
+    return (output[random.randrange(0, len(output))])
+
+@app.route("/donatello")
+def donatello():
+    return (output[random.randrange(0, len(output))])
+
+@app.route("/michelangelo")
+def michelangelo():
+    return (output[random.randrange(0, len(output))])
+
+@app.route("/leonardo")
+def leonardo():
+    return (output[random.randrange(0, len(output))])
 
 
 if __name__ == "__main__":
